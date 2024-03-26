59a9dba85af676a8abbe6cb16b517bfb813225e6
Emakiflom <amankflom23@gmail.com>
2024-03-26 14:04:39 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp
+
+file { '/tmp/school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
